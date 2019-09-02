23988391467a87030dea31dd270e5df3c9394424
tina.wang <wangbl11@qq.com>
2019-09-02 14:32:49 +0800

    commit more

--- sparrow_crawler/views.py
+++ sparrow_crawler/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 
 def index(request,template_name):
-    return render(request,template_name)
+    return render(request,template_name,{"brand_id":request.GET.get('brand_id')})
 
--- static/js/brandlist.js
+++ static/js/brandlist.js
@@ -1,6 +1,6 @@
 $(function () {
     var $wrapper = $('#div-table-container');
-    var $table = $('#task_list_table');
+    var $table = $('#brand_list_table');
 
     var _table = $table.dataTable($.extend(true, {}, CONSTANT.DATA_TABLES.DEFAULT_OPTION, {
         ajax: function (data, callback, settings) {//ajax配置为function,手动调用异步查询
@@ -10,7 +10,7 @@ $(function () {
             var param = productManage.getQueryCondition(data);
             $.ajax({
                 type: "GET",
-                url: "/api/sparrow_crawl/site/o/task/list/",
+                url: "https://backend5.hanguangbaihuo.com/api/sparrow_admin/simple_brands/",
                 cache: false,  //禁用缓存
                 data: param,    //传入已封装的参数
                 dataType: "json",
@@ -43,32 +43,21 @@ $(function () {
             });
         },
         columns: [
-            CONSTANT.DATA_TABLES.COLUMN.CHECKBOX,
             {
                 className: "ellipsis", //文字过长时用省略号显示，CSS实现
-                data: "brand_id",
+                data: "id",
                 width: "45px"
                 //render: CONSTANT.DATA_TABLES.RENDER.ELLIPSIS,//会显示省略号的列，需要用title属性实现划过时显示全部文本的效果
             },
+
             {
-                data: "task_type",
-                width: "80px"
+                data: "name"
             },
             {
-                data: "status",
-                width: "80px"
+                data: "name_en"
             },
             {
-                data: "created_time"
-            }, {
-                data: "finished_time"
-            },
-            {
-                data: "filename",
-                width: "50px",
-                render: function (data, type, row, meta) {
-                    return '<a href="http://www.baidu.com" target="_blank"><i class="fa fa-paperclip"></i></a>';
-                }
+                data: "name_pinyin"
             },
             {
                 className: "td-operation",
@@ -83,41 +72,32 @@ $(function () {
             //给当前行加样式
 
             //不使用render，改用jquery文档操作呈现单元格
-            var $btnView = $('<button type="button" class="btn btn-small btn-primary btn-edit">详情</button>');
-            var $btnDel = $('<button type="button" class="btn btn-small btn-danger btn-del">删除</button>');
-            $('td', row).eq(7).append($btnView).append($btnDel);
 
-        },
-        "drawCallback": function (settings) {
-            //渲染完毕后的回调
-            //清空全选状态
-            $(":checkbox[name='cb-check-all']", $wrapper).prop('checked', false);
-            //默认选中第一行
-            $("tbody tr", $table).eq(0).click();
+            _new='<a href="/api/sparrow_crawl/t/product/list/?brand_id='+data['id']+'">商品</a>';
+            console.log(_new)
+            var $btnView = $(_new);
+            $('td', row).eq(4).append($btnView);
+
         }
     })).api();//此处需调用api()方法,否则返回的是JQuery对象而不是DataTables的API对象
 
-//     $("#btn-add").click(function(){
-//         userManage.addItemInit();
-//     });
-//
-//     $("#btn-del").click(function(){
-//         var arrItemId = [];
-//         $("tbody :checkbox:checked",$table).each(function(i) {
-//             var item = _table.row($(this).closest('tr')).data();
-//             arrItemId.push(item);
-//         });
-//         userManage.deleteItem(arrItemId);
-//     });
-//
-//     $("#btn-simple-search").click(function(){
-//         userManage.fuzzySearch = true;
-//
-//         //reload效果与draw(true)或者draw()类似,draw(false)则可在获取新数据的同时停留在当前页码,可自行试验
-// //      _table.ajax.reload();
-// //      _table.draw(false);
-//         _table.draw();
-//     });
+    _table.on("click", ".btn-products", function () {
+        var item = _table.row($(this).closest('tr')).data();
+        $(this).closest('tr').addClass("active").siblings().removeClass("active");
+        userManage.deleteItem([item]);
+    });
+    $("#btn-simple-search").click(function () {
+
+        //reload效果与draw(true)或者draw()类似,draw(false)则可在获取新数据的同时停留在当前页码,可自行试验
+//      _table.ajax.reload();
+//      _table.draw(false);
+        _table.draw();
+    });
+
+    $("#btn-simple-clear").click(function () {
+        $("#name-search").val('')
+        _table.draw();
+    });
 //
 //     $("#btn-advanced-search").click(function(){
 //         userManage.fuzzySearch = false;
@@ -195,120 +175,19 @@ var productManage = {
         if (data.order && data.order.length && data.order[0]) {
             switch (data.order[0].column) {
                 case 1:
-                    param.orderColumn = "sku";
+                    param.orderColumn = "id";
                     break;
                 default:
-                    param.orderColumn = "title";
+                    param.orderColumn = "name";
                     break;
             }
             param.orderDir = data.order[0].dir;
         }
+        param.name = $("#name-search").val();
         //组装分页参数
-        param.start = data.start;
-        param.length = data.length;
+        param.page = data.start / data.length + 1;
+        param.page_size = data.length;
 
         return param;
     }
 };
-// var userManage = {
-//     currentItem : null,
-//     fuzzySearch : true,
-//     getQueryCondition : function(data) {
-//         var param = {};
-//         //组装排序参数
-//         if (data.order&&data.order.length&&data.order[0]) {
-//             switch (data.order[0].column) {
-//             case 1:
-//                 param.orderColumn = "name";
-//                 break;
-//             case 2:
-//                 param.orderColumn = "position";
-//                 break;
-//             case 3:
-//                 param.orderColumn = "status";
-//                 break;
-//             case 4:
-//                 param.orderColumn = "start_date";
-//                 break;
-//             default:
-//                 param.orderColumn = "name";
-//                 break;
-//             }
-//             param.orderDir = data.order[0].dir;
-//         }
-//         //组装查询参数
-//         param.fuzzySearch = userManage.fuzzySearch;
-//         if (userManage.fuzzySearch) {
-//             param.fuzzy = $("#fuzzy-search").val();
-//         }else{
-//             param.name = $("#name-search").val();
-//             param.position = $("#position-search").val();
-//             param.office = $("#office-search").val();
-//             param.extn = $("#extn-search").val();
-//             param.status = $("#status-search").val();
-//             param.role = $("#role-search").val();
-//         }
-//         //组装分页参数
-//         param.startIndex = data.start;
-//         param.pageSize = data.length;
-//
-//         return param;
-//     },
-//     showItemDetail : function(item) {
-//         $("#user-view").show().siblings(".info-block").hide();
-//         if (!item) {
-//             $("#user-view .prop-value").text("");
-//             return;
-//         }
-//         $("#name-view").text(item.name);
-//         $("#position-view").text(item.position);
-//         $("#salary-view").text(item.salary);
-//         $("#start-date-view").text(item.start_date);
-//         $("#office-view").text(item.office);
-//         $("#extn-view").text(item.extn);
-//         $("#role-view").text(item.role?"管理员":"操作员");
-//         $("#status-view").text(item.status?"在线":"离线");
-//     },
-//     addItemInit : function() {
-//         $("#form-add")[0].reset();
-//
-//         $("#user-add").show().siblings(".info-block").hide();
-//     },
-//     editItemInit : function(item) {
-//         if (!item) {
-//             return;
-//         }
-//         $("#form-edit")[0].reset();
-//         $("#title-edit").text(item.name);
-//         $("#name-edit").val(item.name);
-//         $("#position-edit").val(item.position);
-//         $("#salary-edit").val(item.salary);
-//         $("#start-date-edit").val(item.start_date);
-//         $("#office-edit").val(item.office);
-//         $("#extn-edit").val(item.extn);
-//         $("#role-edit").val(item.role);
-//         $("#user-edit").show().siblings(".info-block").hide();
-//     },
-//     addItemSubmit : function() {
-//         $.dialog.tips('保存当前添加用户');
-//     },
-//     editItemSubmit : function() {
-//         $.dialog.tips('保存当前编辑用户');
-//     },
-//     deleteItem : function(selectedItems) {
-//         var message;
-//         if (selectedItems&&selectedItems.length) {
-//             if (selectedItems.length == 1) {
-//                 message = "确定要删除 '"+selectedItems[0].name+"' 吗?";
-//
-//             }else{
-//                 message = "确定要删除选中的"+selectedItems.length+"项记录吗?";
-//             }
-//             $.dialog.confirmDanger(message, function(){
-//                 $.dialog.tips('执行删除操作');
-//             });
-//         }else{
-//             $.dialog.tips('请先选中要操作的行');
-//         }
-//     }
-// };
